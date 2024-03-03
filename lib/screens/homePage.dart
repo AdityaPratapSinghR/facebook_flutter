9ea283ddf30c7a2fdd94fd09d96388d938ef77df
AdityaPratapSinghR <adityapratap.github@gmail.com>
2024-03-03 18:26:40 +0530

    Made Sign Up Page, Help Page

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_flutter/utils/global.dart';
 import 'package:flutter/material.dart';
 
 class homePage extends StatefulWidget {
@@ -11,10 +12,21 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        titleTextStyle: TextStyle(color: Colors.white,fontSize: 22),
+        title: Text("Facebook"),
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: backgroundColor,
+      ),
       body: SafeArea(
-        child: Container(
+        child: SingleChildScrollView(
 
-        ),
+          child:Column(
+            children: [
+              Image.network("https://images.unsplash.com/photo-1609560140261-4efaa689b6c8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",),
+            ],
+          )
+        )
       ),
     );
   }
